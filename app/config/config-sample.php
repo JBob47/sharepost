54459702fb5ef3f44daa7613b840e7173f2623b1
Justin Bobbitt <justin@justinbobbittdesign.com>
2019-09-13 10:09:18 -0400

    Removed config file and added config sample

---
+++ app/config/config-sample.php
@@ -0,0 +1,18 @@
+<?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'YOUR DATABASE USER');
+    define('DB_PASS', 'YOUR DATABASE PASSWORD');
+    define('DB_NAME' , 'YOUR DATABASE NAME');
+
+    // App root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT', 'https://example.com');
+
+    // Site Name
+    define('SITENAME', 'Sharepost');
+
+    //App Version
+    define('APPVERSION', '1.0.0');
