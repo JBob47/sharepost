16591e13875838550b3e42f683c0f5c06d2ff1a3
Justin Bobbitt <justin@justinbobbittdesign.com>
2019-09-18 15:28:39 -0400

    Changed H1 settings on home page

---
+++ app/config/config-local.php
@@ -0,0 +1,20 @@
+<?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME' , 'traversy-sharepost');
+
+    // App root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT', 'http://localhost/demos/2019/Traversy-Sharepost');
+
+    // Site Name
+    define('SITENAME', 'Sharepost');
+
+    //App Version
+    define('APPVERSION', '1.0.0');
+
+    
